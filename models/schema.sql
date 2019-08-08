0d3a9aa4d58c59df15f07daa213186426fe9a785
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-08-08 16:33:29 -0400

    creating initial wireframe of the website and testing out of if the ccs file links up correctly with the server

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,23 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
-
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+DROP DATABASE IF exists KNOTCODE;
+CREATE DATABASE KNOTCODE;
+USE KNOTCODE;
+​
+CREATE TABLE Topics (
+topicId INTEGER NOT NULL auto_increment,
+topic VARCHAR(50),
+PRIMARY KEY(topicId) 
+);
+​
+CREATE TABLE Questions(
+	id INTEGER NOT NULL auto_increment,
+    topicId integer,
+    question VARCHAR(200),
+    choice1 VARCHAR(200),
+    choice2 VARCHAR(200),
+    choice3 VARCHAR(200),
+    PRIMARY KEY(id),
+    FOREIGN KEY (topicId)
+    REFERENCES KNOTCODE.Topics(topicId)
+); 
+INSERT INTO Topics(topic) VALUES ('HTML'), ('CSS'), ('JavaScript');
+​
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,30 @@
 var db = require("../models");
+var path = require("path");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+  // app.get("/", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExamples
+  //     });
+  //   });
+  // });
+
+  // test
+  app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "index.html"));
+  }) 
+
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -21,7 +32,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
