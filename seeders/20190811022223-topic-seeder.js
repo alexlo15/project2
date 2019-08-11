ec486fe6bde53a81c0a9f70c25c10ac0959d0f2e
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-08-11 00:30:22 -0400

    tested the topic seeder to see if it would populate the initial data for the database. started to create the question seeder

--- seeders/20190811022223-topic-seeder.js
+++ seeders/20190811022223-topic-seeder.js
@@ -2,25 +2,18 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('Topic', [{
+        name: 'HTML'
+      }, 
+      {
+        name: 'CSS'
+      },
+      {
+        name: 'Javascript'
+      }
+    ], {});
+
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
 };
