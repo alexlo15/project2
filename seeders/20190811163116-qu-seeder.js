317514fc2c7f51ddb6d38666430e29ead4531bbe
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-08-11 12:38:33 -0400

    testing to see if seeding works for alex

--- seeders/20190811163116-qu-seeder.js
+++ seeders/20190811163116-qu-seeder.js
@@ -41,7 +41,7 @@ module.exports = {
         choice2: 'Hastly Text Markup Language',
         choice3: 'Handlebar Text Markup Language',
         skillLevel: 1,
-        topicId: 1
+        topicId: 2
       },
     ], {});
 
