29fcea9f3df4108f2cf808fcf3dd911b25aceb27
Alex Lo <loalexander389@gmail.com>
2019-08-12 13:52:43 -0400

    front end js, some css, trying to make skill level work

--- public/js/flash-card.js
+++ public/js/flash-card.js
@@ -1,32 +1,70 @@
-$(document).ready(function(){
-    // alert('flash-card attached');
-    var topics = localStorage.getItem('vTopicSelected');  
-    // alert("local Storage value: "+ topics);
+$(document).ready(function () {
+  // alert('flash-card attached');
+  var topics = localStorage.getItem('vTopicSelected');
+  var level = localStorage.getItem("SkillLvl");
+  // alert("local Storage value: "+ topics);
 
-    function getQuestions(topics) {
-        let topicId = [];
-        // alert(topics);
-        switch(topics){
-            case 'HTML':
-                topicId.push('1');
-                break;
+
+  function getQuestions(topics) {
+    let topicId = [];
+    // alert(topics);
+    switch (topics) {
+      case 'HTML':
+        switch (level) {
+          case '1':
+            topicId.push('1');
+            break;
+          case '2':
+            topicId.push('2');
+            break;
+          case '3':
+            topicId.push('3');
+            break;
         }
-        $.get("/api/questions/"+topicId, function(data) {
-          var questions=[];
-          var answers=[];
-          for (var i = 0; i < data.length; i++) {
-           questions.push((data[i].question));
-           answers.push(data[i].choice1);
-          }
-
-          $('#question').html(questions);
-          $('#answers').html(answers);
-         
-        // console.log(data);
-        // console.log(questions);
-        // console.log(answers);
-        });
+        break;
+      case 'CSS':
+        switch (level) {
+          case '1':
+            topicId.push('4');
+            break
+          case '2':
+            topicId.push('5');
+            break
+          case '3':
+            topicId.push('6');
+            break
+        }
+        break;
+      case 'JS':
+        switch (level) {
+          case '1':
+            topicId.push('7');
+            break
+          case '2':
+            topicId.push('8');
+            break
+          case '3':
+            topicId.push('9');
+            break
+        }
+        break;
+    }
+    $.get("/api/questions/" + topicId, function (data) {
+      var questions = [];
+      var answers = [];
+      for (var i = 0; i < data.length; i++) {
+        questions.push((data[i].question));
+        answers.push(data[i].choice1);
       }
-getQuestions(topics);
+
+      $('#question').html(questions);
+      $('#answers').html(answers);
+
+      // console.log(data);
+      // console.log(questions);
+      // console.log(answers);
+    });
+  }
+  getQuestions(topics);
 
 });
