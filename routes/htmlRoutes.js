81ed32c2bcaa8f492fd14ca21ed1cdd310e010b1
Will Stark <wnstark@gmail.com>
2019-08-14 13:47:41 -0400

    flash card style

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -25,7 +25,7 @@ module.exports = function (app) {
   }) 
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
+  // app.get("*", function (req, res) {
+  //   res.render("404");
+  // });
 };
